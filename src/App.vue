ae583796a1693c55ede30c76392c66557d9148cc
hiroko-ino <los73.butterfly.1251.12.15@gmail.com>
2024-11-16 23:38:18 +0900

    make site

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,181 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import CursorArea from './components/CursorArea.vue'
+
+const CATEGORIES = {
+  general: 'General',
+  linkAndStatus: 'Links & status',
+  selection: 'Selection',
+  dragAndDrop: 'Drag & drop',
+  resizingAndScrolling: 'Resizing & scrolling',
+  zooming: 'Zooming',
+}
+
+const items = [
+  {
+    type: 'auto',
+    category: CATEGORIES.general,
+  },
+  {
+    type: 'default',
+    category: CATEGORIES.general,
+  },
+  {
+    type: 'none',
+    category: CATEGORIES.general,
+  },
+  {
+    type: 'context-menu',
+    category: CATEGORIES.linkAndStatus,
+  },
+  {
+    type: 'help',
+    category: CATEGORIES.linkAndStatus,
+  },
+  {
+    type: 'pointer',
+    category: CATEGORIES.linkAndStatus,
+  },
+  {
+    type: 'progress',
+    category: CATEGORIES.linkAndStatus,
+  },
+  {
+    type: 'wait',
+    category: CATEGORIES.linkAndStatus,
+  },
+  {
+    type: 'cell',
+    category: CATEGORIES.selection,
+  },
+  {
+    type: 'crosshair',
+    category: CATEGORIES.selection,
+  },
+  {
+    type: 'text',
+    category: CATEGORIES.selection,
+  },
+  {
+    type: 'vertical-text',
+    category: CATEGORIES.selection,
+  },
+  {
+    type: 'alias',
+    category: CATEGORIES.dragAndDrop,
+  },
+  {
+    type: 'copy',
+    category: CATEGORIES.dragAndDrop,
+  },
+  {
+    type: 'move',
+    category: CATEGORIES.dragAndDrop,
+  },
+  {
+    type: 'no-drop',
+    category: CATEGORIES.dragAndDrop,
+  },
+  {
+    type: 'not-allowed',
+    category: CATEGORIES.dragAndDrop,
+  },
+  {
+    type: 'grab',
+    category: CATEGORIES.dragAndDrop,
+  },
+  {
+    type: 'grabbing',
+    category: CATEGORIES.dragAndDrop,
+  },
+  {
+    type: 'all-scroll',
+    category: CATEGORIES.resizingAndScrolling,
+  },
+  {
+    type: 'col-resize',
+    category: CATEGORIES.resizingAndScrolling,
+  },
+  {
+    type: 'row-resize',
+    category: CATEGORIES.resizingAndScrolling,
+  },
+  {
+    type: 'n-resize',
+    category: CATEGORIES.resizingAndScrolling,
+  },
+  {
+    type: 'e-resize',
+    category: CATEGORIES.resizingAndScrolling,
+  },
+  {
+    type: 's-resize',
+    category: CATEGORIES.resizingAndScrolling,
+  },
+  {
+    type: 'w-resize',
+    category: CATEGORIES.resizingAndScrolling,
+  },
+  {
+    type: 'ne-resize',
+    category: CATEGORIES.resizingAndScrolling,
+  },
+  {
+    type: 'nw-resize',
+    category: CATEGORIES.resizingAndScrolling,
+  },
+  {
+    type: 'se-resize',
+    category: CATEGORIES.resizingAndScrolling,
+  },
+  {
+    type: 'sw-resize',
+    category: CATEGORIES.resizingAndScrolling,
+  },
+  {
+    type: 'ew-resize',
+    category: CATEGORIES.resizingAndScrolling,
+  },
+  {
+    type: 'ns-resize',
+    category: CATEGORIES.resizingAndScrolling,
+  },
+  {
+    type: 'nesw-resize',
+    category: CATEGORIES.resizingAndScrolling,
+  },
+  {
+    type: 'nwse-resize',
+    category: CATEGORIES.resizingAndScrolling,
+  },
+  {
+    type: 'zoom-in',
+    category: CATEGORIES.zooming,
+  },
+  {
+    type: 'zoom-out',
+    category: CATEGORIES.zooming,
+  }
+]
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="wrapper">
+    <h1 class="heading">Which cursor type do you like?</h1>
+    <CursorArea
+      v-for="(item, i) in items"
+      :key="item.type"
+      :color="i % 2 === 0 ? '#E5D9F2' : '#F5EFFF'"
+      :cursor-type="item.type"
+      :category="item.category"
+    />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.heading {
+  text-align: center;
+  padding: 0.2rem 0 1.4rem;
+  font-size: 2rem;
+  margin: 0;
 }
 </style>
---
+++ src/components/CursorArea.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+defineProps<{ cursorType: string, color: string, category: string }>()
+</script>
+
+<template>
+  <div class="cursor-area" :style="{ cursor: cursorType, backgroundColor: color }">
+    <div>
+      <h2 class="cursor-area__category">{{ category }}</h2>
+      <span class="cursor-area__type">cursor: {{ cursorType }}</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.cursor-area {
+  height: 300px;
+  display: grid;
+  place-items: center;
+}
+
+.cursor-area__category {
+  font-size: 1.2rem;
+  margin-bottom: 0.2rem;
+  color: #888;
+}
+
+.cursor-area__type {
+  font-size: 1.4rem;
+  color: #333;
+  font-weight: bold;
+}
+</style>
